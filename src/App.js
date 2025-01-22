a434905dab6671db4bf25e1e564f532d985a0df8
ShirleyXW <shirley.wang.xl@outlook.com>
2025-01-21 21:00:07 -0800

    initial pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import"./index.css";
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/home';
+import About from './routes/about';
+import Contact from './routes/contact';
+import Project from './routes/project';
 
 function App() {
   return (
     <>
-    <div>
-     <h1>My React App</h1>
-    </div>
+  
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/project" element={<Project />} />
+    </Routes>
+
     </>
   );
 }
